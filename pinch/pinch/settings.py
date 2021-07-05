bc295afd30325c243bf2133bcb930dbf56217e3d
Haejin <hj0816hj@gmail.com>
2021-07-06 00:23:48 +0900

    first migration

--- pinch/pinch/settings.py
+++ pinch/pinch/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+with open(os.path.join(BASE_DIR, 'secrets.json'), 'rb') as secret_file:
+    secrets = json.load(secret_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f0u5^&jx3bb2b$2y94!)ve%@fz3#1sctl@4$kdau+9@u6+e695'
+SECRET_KEY = secrets['SECRET_KEY']  # Hide the secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +76,7 @@ WSGI_APPLICATION = 'pinch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = secrets['DB_SETTINGS']
 
 
 # Password validation
