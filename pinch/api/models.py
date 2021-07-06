a6bd656f86e3a686fbd190aff3ae37f8393fc9c1
Haejin <hj0816hj@gmail.com>
2021-07-07 01:16:10 +0900

    add models

--- pinch/api/models.py
+++ pinch/api/models.py
@@ -1,3 +1,72 @@
 from django.db import models
-
 # Create your models here.
+
+'''
+log(생성 시각, 수정 시각)를 위해 모든 모델에서 사용하는 Base model
+'''
+
+
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+'''
+사용자
+'''
+
+
+class User(Base):
+    name = models.CharField()
+    email_address = models.CharField(unique=True)
+    num_of_subs = models.IntegerField(default=0)
+    num_of_bookmarks = models.IntegerField(default=0)
+    last_login_time = models.DateTimeField()
+    # TO_DO : 프로필 사진
+    # TO_DO : 사용자 access_token, refresh_token 저장
+
+    def __str__(self):
+        return self.email_address
+
+
+'''
+뉴스
+'''
+
+
+class Subscribe(Base):
+    user = models.ManyToManyField(User)
+    name = models.CharField()
+    email_address = models.CharField(unique=True)
+
+    def __str__(self):
+        return self.email_address
+
+
+'''
+사용자가 구독한 뉴스를 연결해주는 매개 테이블
+'''
+
+
+class UserSubscribe(Base):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subscribe = models.ForeignKey(Subscribe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{}->{}'.format(self.user.email_address, self.subscribe.email_address)
+
+
+'''
+사용자가 북마크한 이메일 정보
+'''
+
+
+class Bookmark(Base):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    email_id = models.IntegerField()
+
+    def __str__(self):
+        return '{}->{}'.format(self.user.email_address, self.email_id)
