5358a200bbc0b2a3c49ec40efd673c053ecb2249
Haejin <hj0816hj@gmail.com>
2021-07-16 00:45:07 +0900

    feat: add api urls

--- pinch/api/urls.py
+++ pinch/api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from rest_framework import routers
 from . import views
 
 app_name = 'api'
 
-urlpatterns = [
-    path('email_senders', views.email_senders),
+router = routers.DefaultRouter()
+router.register(r'subscription', views.SubscriptionViewSet)
+router.register(r'bookmark', views.BookmarkViewSet)
+
+urlpatterns = router.urls + [
+    path('email', views.email),
+    path('user/email-senders', views.email_senders),
 ]
