93899984a260db79149af52d3f6bba2b8f6435d6
Haejin <hj0816hj@gmail.com>
2021-07-18 17:48:05 +0900

    feat: add unique in bookmark

--- pinch/api/models.py
+++ pinch/api/models.py
@@ -27,7 +27,7 @@ class User(Base):
     # num_of_bookmarks = models.IntegerField(default=0)
     last_email_time = models.DateTimeField(null=True)
     # TO_DO : 프로필 사진
-    label_id = models.CharField(max_length=100, unique=True)
+    # label_id = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.email_address
@@ -73,7 +73,7 @@ class UserSubscription(Base):
 
 class Bookmark(Base):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    email_id = models.CharField(max_length=100)
+    email_id = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return '{}->{}'.format(self.user.email_address, self.email_id)
